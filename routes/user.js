60c9af5b8eab483ef74a58b788f78fa908789367
mnz <muneeskarakkad62@gmail.com>
2023-07-04 19:56:07 +0530

    model schema created for cart order product user

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("user test is successfull");
-});
 
-router.post("/userposttest",(req,res)=>{
-    const username=req.body.username;
-    console.log(username);
-    res.send("your username is :"+username)
-})
 
 module.exports = router;
+
+
+
+
+
+
+
+// Study Routes
+
+// router.get("/usertest", (req, res) => {
+//   res.send("user test is successfull");
+// });
+
+// router.post("/userposttest",(req,res)=>{
+//     const username=req.body.username;
+//     console.log(username);
+//     res.send("your username is :"+username)
+// })
