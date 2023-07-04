7dd16cb73c24b44a307b5e774cb4913d229ae8cd
mnz <muneeskarakkad62@gmail.com>
2023-07-04 19:38:25 +0530

    understanding about routes in express

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const app = express();
-
 const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
+const userRoute=require('./routes/user')
 
 dotenv.config();
 
@@ -16,6 +14,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// app.get('/api/test',()=>{
+//     console.log("test is successfull")
+// })
+
+app.use(express.json())
+
+app.use("/api/user",userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is Running!");
