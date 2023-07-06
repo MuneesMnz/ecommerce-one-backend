3684ea9c6c988eceadf5af850a26ea6826849c1d
mnz <muneeskarakkad62@gmail.com>
2023-07-06 13:21:03 +0530

    backend completed

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute=require('./routes/auth')
 const productRoute=require('./routes/product')
 const cartRoute=require('./routes/cart')
 const orderRoute=require('./routes/order')
+const stripeRoute=require('./routes/stripe')
+const cors=require("cors")
 
 dotenv.config();
 
@@ -23,6 +25,8 @@ mongoose
 //     console.log("test is successfull")
 // })
 
+app.use(cors())
+
 app.use(express.json())
 
 app.use("/api/auth",authRoute)
@@ -30,6 +34,7 @@ app.use("/api/user",userRoute)
 app.use("/api/product",productRoute)
 app.use("/api/cart",cartRoute)
 app.use("/api/order",orderRoute)
+app.use("/api/stripe",stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is Running!");
