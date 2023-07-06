e7c3d0ef1de7b2999a0942581db533914ca86cb5
mnz <muneeskarakkad62@gmail.com>
2023-07-06 10:54:16 +0530

    crud operation completed on order and cart

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const userRoute=require('./routes/user')
 const authRoute=require('./routes/auth')
 const productRoute=require('./routes/product')
+const cartRoute=require('./routes/cart')
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use("/api/auth",authRoute)
 app.use("/api/user",userRoute)
 app.use("/api/product",productRoute)
+app.use("/api/cart",cartRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is Running!");
